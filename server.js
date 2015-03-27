a4aa3902552f92f86a8fd475e8456c08bc45a595
Thomas Lanciaux <thomaslanciaux4@gmail.com>
2015-03-27 12:33:55 +0000

    Enable CORS for score demo server

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ var express = require('express');
 
 var app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", 
+             "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.post('/score', function(req, res) {
   setTimeout(function() {
     res.send('Scored');
